6a83b5b074708f1e76f0e1b9bea0bab88cb78918
saumishr <saumishr@www.domain1.com>
2014-02-13 00:39:55 +0530

    Additional changes for user repitition in follower/following list

--- relationships/templatetags/relationship_tags.py
+++ relationships/templatetags/relationship_tags.py
@@ -343,7 +343,7 @@ def render_follower_subset(context, user_obj, sIndex, lIndex, data_chunk):
     data_href = reverse('get_follower_subset', kwargs={
             'content_type_id': content_type, 'object_id': user_obj.pk, 'sIndex':sIndex, 'lIndex':lIndex})
     return template.render(RequestContext(context['request'], {
-            "friends": user_obj.relationships.followers()[sIndex:lIndex],
+            "friends": user_obj.relationships.followers().order_by('-date_joined')[sIndex:lIndex],
             'is_incremental': False,
             'data_href': data_href,
             'data_chunk': data_chunk,
@@ -359,7 +359,7 @@ def render_following_subset(context, user_obj, sIndex, lIndex, data_chunk):
     data_href = reverse('get_following_subset', kwargs={
             'content_type_id': content_type, 'object_id': user_obj.pk, 'sIndex':sIndex, 'lIndex':lIndex})
     return template.render(RequestContext(context['request'], {
-            "friends": user_obj.relationships.following()[sIndex:lIndex],
+            "friends": user_obj.relationships.following().order_by('-date_joined')[sIndex:lIndex],
             'is_incremental': False,
             'data_href':data_href,
             'data_chunk': data_chunk,
